cdc8b70fdbef88b32fab50ce83c4199936358682
MsMcKee11 <cabothief@gmail.com>
2021-11-22 17:32:38 -0800

    Now with books??

---
+++ data/temp-data.1637630971808.js
@@ -0,0 +1,1 @@
+window.tempDataJSON = {"books":true,"series":true,"collections":true,"wishlist":true,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":true},{"key":"authors","enabled":true},{"key":"publishers","enabled":true},{"key":"series","enabled":true},{"key":"narrators","enabled":true}],"cacheID":1637630971808}};
